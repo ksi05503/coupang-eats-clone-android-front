cdf6d567b8cc423da5082298537d1145759eef9c
hwkim98 <ksi05503@gmail.com>
2021-08-10 17:07:36 +0900

    레스토랑 액티비티 추가

--- app/src/main/java/com/example/risingtest/src/main/home/RestaurantTypeAdapter.kt
+++ app/src/main/java/com/example/risingtest/src/main/home/RestaurantTypeAdapter.kt
@@ -1,14 +1,17 @@
 package com.example.risingtest.src.main.home
 
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.risingtest.R
 import com.example.risingtest.src.main.RestaurantType
+import com.example.risingtest.src.main.restaurant.RestaurantActivity
 
 //  adapter.notifyDataSetChanged() 데이터 리스트 변화있을때 이거하면 업데이트 된다.
 
@@ -25,7 +28,13 @@ class RestaurantTypeAdapter(): RecyclerView.Adapter<RestaurantTypeAdapter.ViewHo
         }
 
         init {
+
             view.setOnClickListener {
+                //어댑터에서 인텐트로 startActivity쏠때는 앞에 컨텍스트가 붙어야함(어댑터호출하는 상위액티비티)
+                val i = Intent(view.context, RestaurantActivity::class.java)
+                view.context.startActivity(i)
+
+
                 Log.d("Rtype recyclerView","뷰 눌렸습니다.")
             }
         }
---
+++ app/src/main/java/com/example/risingtest/src/main/restaurant/RestaurantActivity.kt
@@ -0,0 +1,13 @@
+package com.example.risingtest.src.main.restaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.risingtest.R
+import com.example.risingtest.config.BaseActivity
+import com.example.risingtest.databinding.ActivityRestaurantBinding
+
+class RestaurantActivity : BaseActivity<ActivityRestaurantBinding>(ActivityRestaurantBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
